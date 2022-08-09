d8edd68af55651aa92e21f3c048c974a0e4ac586
Oleksandr Dashko <leksdashko@gmail.com>
2022-08-09 19:58:11 +0200

    movie structure finoshed

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,11 +1,18 @@
 import React, {useState, useEffect} from 'react';
+import { Grid } from '@mui/material';
+import { Movie } from '..';
 
-const MovieList = () => {
+import useStyles from './styles';
+
+const MovieList = ({movies}) => {
+	const classes = useStyles();
 
 	return (
-		<div>
-			MovieList
-		</div>
+		<Grid container className={classes.moviesContainer}>
+			{movies.results.map((movie, i) => (
+				<Movie key={i} movie={movie} i={i} />
+			))}
+		</Grid>
 	);
 };
 
--- src/components/MovieList/styles.js
+++ src/components/MovieList/styles.js
@@ -1,7 +1,13 @@
 import { makeStyles } from "@mui/styles";
 
 export default makeStyles((theme) => ({
-	toolbar: {
-		
+	moviesContainer: {
+		display: 'flex',
+		flexWrap: 'wrap',
+		justifyContent: 'space-between',
+		overflow: 'auto',
+		[theme.breakpoints.down('sm')]: {
+			justifyContent: 'center'
+		}
 	}
 }));
